aa103ee8247029787850bacb660f742854a70310
IablonschiEduard <s188800@uos.ac.uk>
2017-10-05 01:31:00 +0300

    message

---
+++ ex4.py
@@ -0,0 +1,15 @@
+my_name = 'Iablonschi Eduard'
+my_age = 19
+my_height = 180 # centimeters, I don't know my height in inches
+my_weight = 90 # kilograms, I don't know my weight in pounds either
+my_eyes = 'Brown'
+my_hair = 'Black'
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
